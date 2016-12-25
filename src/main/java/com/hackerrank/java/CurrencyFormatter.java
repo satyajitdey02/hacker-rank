7ea89acd2dd9e048ac859b94d4e4420d9933c4ee
Satyajit Dey <satyajit02@gmail.com>
2016-12-25 21:11:46 +0600

    HR-JAVA: Currency Format solved

---
+++ src/main/java/com/hackerrank/java/CurrencyFormatter.java
@@ -0,0 +1,32 @@
+package com.hackerrank.java;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by satyajit on 12/25/16.
+ */
+public class CurrencyFormatter {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double input = scanner.nextDouble();
+    scanner.close();
+
+    if(input < 0 || input > Math.pow(10, 9)) {
+      return;
+    }
+
+    NumberFormat currencyInstance = NumberFormat.getCurrencyInstance(Locale.US);
+    System.out.printf("US: %s\n", currencyInstance.format(input));
+
+    currencyInstance = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+    System.out.printf("India: %s\n", currencyInstance.format(input));
+
+    currencyInstance = NumberFormat.getCurrencyInstance(Locale.CHINA);
+    System.out.printf("China: %s\n", currencyInstance.format(input));
+
+    currencyInstance = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+    System.out.printf("France: %s\n", currencyInstance.format(input));
+  }
+}
