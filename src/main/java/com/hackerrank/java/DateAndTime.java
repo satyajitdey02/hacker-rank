6960f52ca04b863607a0611f278dd0a9b5a75a98
Satyajit Dey <satyajit02@gmail.com>
2016-12-25 19:34:07 +0600

    HR-JAVA: Date and Time solved

---
+++ src/main/java/com/hackerrank/java/DateAndTime.java
@@ -0,0 +1,28 @@
+package com.hackerrank.java;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created by satyajit on 12/25/16.
+ */
+public class DateAndTime {
+  public static void main(String[] args) {
+    String[] daysOfWeek = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+    Scanner scanner = new Scanner(System.in);
+    int mm = scanner.nextInt();
+    int dd = scanner.nextInt();
+    int yyyy = scanner.nextInt();
+    if (yyyy <= 2000 || yyyy >= 3000) {
+      return;
+    }
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(yyyy, mm - 1, dd);
+
+    System.out.println(daysOfWeek[calendar.get(calendar.DAY_OF_WEEK) - 1]);
+    scanner.close();
+  }
+}
