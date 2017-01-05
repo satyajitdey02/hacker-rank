d7375817cecf0b2b64a01f9feffb5f9f02f72127
Satyajit Dey <satyajit02@gmail.com>
2017-01-06 00:40:24 +0600

    HR-DS: Print Reverse Garbage Code

--- src/main/java/com/hackerrank/datastructures/linkedlist/LinkedListPrinter.java
+++ src/main/java/com/hackerrank/datastructures/linkedlist/LinkedListPrinter.java
@@ -6,16 +6,33 @@ package com.hackerrank.datastructures.linkedlist;
 public class LinkedListPrinter {
   private Node head;
 
+  public static void main(String[] args) {
+    //InsertNode
+  }
+
   public LinkedListPrinter(Node head) {
     this.head = head;
   }
 
   /*Print the Elements of a Linked List*/
-  public void print() {
+  void print() {
     while (head != null) {
       System.out.println(head.data);
       head = head.next;
     }
+  }
+
+  void printReverse(Node head) {
+    if(head == null) {
+      return;
+    }
+
+    if(head.next == null) {
+      System.out.println(head.data);
+    }
+
+    printReverse(head.next);
+
   }
 
 }
