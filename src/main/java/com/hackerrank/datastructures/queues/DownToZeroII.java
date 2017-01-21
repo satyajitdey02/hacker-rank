0b7aed82c48de8bfc1168d973e2e08c070cdde90
Satyajit Dey <satyajit02@gmail.com>
2017-01-21 20:19:42 +0600

    HR-DS: Add all factors finding code

--- src/main/java/com/hackerrank/datastructures/queues/DownToZeroII.java
+++ src/main/java/com/hackerrank/datastructures/queues/DownToZeroII.java
@@ -1,90 +1,58 @@
 package com.hackerrank.datastructures.queues;
 
-import com.hackerrank.utils.MathUtils;
-
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * Created by satyajit on 1/21/17.
  */
 public class DownToZeroII {
-  static Map<Integer, Integer> leastDiffFactorsMap = new HashMap<>();
+  static Map<Integer, Integer> allFactorsMap;
 
   public static void main(String[] args) {
-    //Scanner in = new Scanner(System.in);
-    //int q = in.nextInt();
-    //while (q-- > 0) {
-      int n = 603900;//in.nextInt();
-      int move = reduce(n);
-      System.out.println(move);
-      //int move2 = reduce(n - 1);
-      //System.out.println(move2);
-      //System.out.print(n + "-->");
-
-
-      //System.out.println(move > move2 ? move2 : move);
-    //}
-    //in.close();
-
-  }
-
-  private static int reduce(int n) {
-    int move = 0;
-    //System.out.print(n + "-->");
-    while (n != 0) {
-      if (n == 1) {
-        // System.out.print("1-1>-->0");
-        move++;
-        break;
+    Scanner in = new Scanner(System.in);
+    try {
+      while (true) {
+        int n = in.nextInt();
+        reduce(n);
       }
-
-      if (isPrime(n)) {
-        //System.out.print(n + "-1-->");
-        n = n - 1;
-        move++;
-        continue;
-      }
-
-      //int fA = largestPrimeFactorOf(n);
-      //int fB = n / fA;
-      n = leastDiffMaxFactor(n);//Math.max(fA, fB);
-      //System.out.print(String.format("max(%d, %d)-->", fA, fB));
-
-      move++;
+    } catch (Exception e) {
+      System.out.println("Input Aborted!");
+    } finally {
+      in.close();
     }
-    return move;
   }
 
-  private static int leastDiffMaxFactor(int n) {
-    if (leastDiffFactorsMap.get(n) != null) {
-      return leastDiffFactorsMap.get(n);
+  private static void reduce(int n) {
+    System.out.print("-->" + n);
+    if (n == 0) {
+      System.out.println();
+      return;
+    }
+    if (n == 1) {
+      reduce(0);
+      return;
+    }
+    if (n == 2) {
+      reduce(1);
+      return;
+    }
+    if (n == 3) {
+      reduce(2);
+      return;
     }
 
     if (isPrime(n)) {
-      leastDiffFactorsMap.put(n, n);
-      return n;
+      reduce(n - 1);
+      return;
     }
 
-    Map<Integer, Integer> map = new HashMap<>();
-    int diff = Integer.MAX_VALUE;
-    int result = n;
-    for (int f1 = 2; f1 < Math.sqrt(n) + 1; f1++) {
-      //for (int f1 = (int)Math.sqrt(n); f1 <= n; f1++) {
-      if (n % f1 == 0) {
-        int f2 = n / f1;
-        result = Math.max(f1, f2);
-        /*if (diff > Math.abs(f1 - f2)) {
-          diff = Math.abs(f1 - f2);
-          result = Math.max(f1, f2);
-        }*/
-
-      }
+    findAllFactors(n);
+    for (Map.Entry<Integer, Integer> entry : allFactorsMap.entrySet()) {
+      int maxFactor = Math.max(entry.getKey(), entry.getValue());
+      reduce(maxFactor);
     }
-
-    leastDiffFactorsMap.put(n, result);
-    return result;
   }
 
   public static boolean isPrime(int number) {
@@ -108,15 +76,18 @@ public class DownToZeroII {
     return true;
   }
 
-  public static int largestPrimeFactorOf(int number) {
-    int result;
-    for (result = 2; result <= number; result++) {
-      if (number % result == 0) {
-        number = number / result;
-        result--;
+  private static void findAllFactors(int n) {
+    allFactorsMap = new TreeMap<>();
+    for (int i = 2; i * i <= n; i++) {
+      if (n % i == 0) {
+        allFactorsMap.put(i, n / i);
       }
     }
+  }
 
-    return result;
+  private static void printALlFactors() {
+    for (Map.Entry<Integer, Integer> entry : allFactorsMap.entrySet()) {
+      System.out.printf("((%d, %d)%n", entry.getKey(), entry.getValue());
+    }
   }
 }
