0f527f46536fad127f53947f433bda778e225156
Satyajit Dey <satyajit.dey@sharebox.no>
2022-03-05 20:26:49 +0600

    KAYAK-EX#1: Overload find method

--- src/main/java/com/skill/test/kayak/RobotDestinationFinder.java
+++ src/main/java/com/skill/test/kayak/RobotDestinationFinder.java
@@ -8,17 +8,21 @@ public class RobotDestinationFinder {
      * @return an Array of Integers with 2 elements representing the calculated coordinates of the Robot
      */
     public Integer[] find(String instructions) {
+        return find(instructions, Turn.LEFT, Turn.RIGHT, 1);
+    }
+
+    public Integer[] find(String instructions, Turn leftDirection, Turn rightDirection, int moveForwardUnit) {
         int x = 0, y = 0;
         double theta = 0.0;
 
         for (int i = 0; i < instructions.length(); i++) {
             if (instructions.charAt(i) == 'L') {
-                theta = theta - 90.0;
+                theta = theta + (leftDirection.getValue() * 90.0);
             } else if (instructions.charAt(i) == 'R') {
-                theta = theta + 90.0;
+                theta = theta + (rightDirection.getValue() * 90.0);
             } else {//Consider instructions are valid containing characters 'L', 'R' and 'F' only
-                x = x + (int) Math.sin(Math.toRadians(theta));
-                y = y + (int) Math.cos(Math.toRadians(theta));
+                x = x + (int) Math.sin(Math.toRadians(theta)) * moveForwardUnit;
+                y = y + (int) Math.cos(Math.toRadians(theta)) * moveForwardUnit;
             }
         }
 
