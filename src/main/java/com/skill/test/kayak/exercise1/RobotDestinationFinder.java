cb27d8a1876fb1f286247059dcd6d264603bb0f0
Satyajit Dey <satyajit.dey@sharebox.no>
2022-03-06 01:51:56 +0600

    KAYAK- Implement Simple RobotDestinationFinder

--- src/main/java/com/skill/test/kayak/exercise1/RobotDestinationFinder.java
+++ src/main/java/com/skill/test/kayak/exercise1/RobotDestinationFinder.java
@@ -1,9 +1,6 @@
 package com.skill.test.kayak.exercise1;
 
-import com.skill.test.kayak.exercise3.Turn;
-
 public class RobotDestinationFinder {
-
 
     /**
      * <p>Find the final destination of a robot based on the movement {instructions}</p>
@@ -11,30 +8,17 @@ public class RobotDestinationFinder {
      * @return an Array of Integers with 2 elements representing the calculated coordinates of the Robot
      */
     public Integer[] find(String instructions) {
-        return find(instructions, Turn.LEFT, Turn.RIGHT, 1);
-    }
-
-    /**
-     * <p>Find the final destination of a robot based on the movement {instructions} and
-     * {leftDirection}, {rightDirection}, {moveForwardUnit} configurations</p>
-     * @param instructions a String containing characters L(Turn Left), R(Turn Right) or F(Go Forward)
-     * @param leftDirection Turn left configuration for misguided robots
-     * @param rightDirection Turn right configuration for misguided robots
-     * @param moveForwardUnit Move forward configuration for turbo/sluggish robots
-     * @return an Array of Integers with 2 elements representing the calculated coordinates of the Robot
-     */
-    public Integer[] find(String instructions, Turn leftDirection, Turn rightDirection, int moveForwardUnit) {
         int x = 0, y = 0;
         double theta = 0.0;
 
         for (int i = 0; i < instructions.length(); i++) {
             if (instructions.charAt(i) == 'L') {
-                theta = theta + (leftDirection.getValue() * 90.0);
+                theta = theta - 90.0;
             } else if (instructions.charAt(i) == 'R') {
-                theta = theta + (rightDirection.getValue() * 90.0);
-            } else {//Consider instructions are valid containing characters 'L', 'R' and 'F' only
-                x = x + (int) Math.sin(Math.toRadians(theta)) * moveForwardUnit;
-                y = y + (int) Math.cos(Math.toRadians(theta)) * moveForwardUnit;
+                theta = theta + 90.0;
+            } else {
+                x = x + (int) Math.sin(Math.toRadians(theta));
+                y = y + (int) Math.cos(Math.toRadians(theta));
             }
         }
 
