ba887bd180a3ab1c7f370617577100962b4dc1bd
Satyajit Dey <satyajit.dey@sharebox.no>
2022-03-06 00:04:52 +0600

    KAYAK-EX#1: Add javadocs to overloaded method

--- src/main/java/com/skill/test/kayak/RobotDestinationFinder.java
+++ src/main/java/com/skill/test/kayak/RobotDestinationFinder.java
@@ -4,6 +4,7 @@ public class RobotDestinationFinder {
 
 
     /**
+     * <p>Find the final destination of a robot based on the movement {instructions}</p>
      * @param instructions a String containing characters L(Turn Left), R(Turn Right) or F(Go Forward)
      * @return an Array of Integers with 2 elements representing the calculated coordinates of the Robot
      */
@@ -11,6 +12,15 @@ public class RobotDestinationFinder {
         return find(instructions, Turn.LEFT, Turn.RIGHT, 1);
     }
 
+    /**
+     * <p>Find the final destination of a robot based on the movement {instructions} and
+     * {leftDirection}, {rightDirection}, {moveForwardUnit} configurations</p>
+     * @param instructions a String containing characters L(Turn Left), R(Turn Right) or F(Go Forward)
+     * @param leftDirection Turn left configuration for misguided robots
+     * @param rightDirection Turn right configuration for misguided robots
+     * @param moveForwardUnit Move forward configuration for turbo/sluggish robots
+     * @return an Array of Integers with 2 elements representing the calculated coordinates of the Robot
+     */
     public Integer[] find(String instructions, Turn leftDirection, Turn rightDirection, int moveForwardUnit) {
         int x = 0, y = 0;
         double theta = 0.0;
